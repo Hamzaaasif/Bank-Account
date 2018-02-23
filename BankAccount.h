1c6ce947b6d32e6c7067b692ba0a3e0d9384c721
Hamza Asif <hamzaasif0987@gmail.com>
2018-02-23 15:29:01 +0500

    Back to Original file

--- BankAccount.h
+++ BankAccount.h
@@ -6,16 +6,21 @@ class BankAccount
   BankAccount()
   {
     this->AccountHolder = "Hamza Asif";
-    this->AccountNumber = 1121211;
+    this->AccountNumber = 422018976520;
     this->CurrentBalance = 78675.70;
-  };
+  }
   BankAccount(string AccHolder,int AccNumber,float CurrentBal)
   {
     this->AccountHolder = AccHolder;
     this->AccountNumber = AccNumber;
     this->CurrentBalance = CurrentBal;
-  };
-  
+  }
+  BankAccount(BankAccount& cloneAcc)
+  {
+    this->AccountHolder = cloneAcc.AccountHolder;
+    this->AccountNumber = cloneAcc.AccountNumber;
+    this->CurrentBalance = cloneAcc.CurrentBalance;
+  }
   void display()
   {
     cout<<this->AccountHolder<<endl<<this->AccountNumber<<endl<<this->CurrentBalance<<endl;
@@ -24,4 +29,4 @@ class BankAccount
   string AccountHolder;
   int AccountNumber;
   float CurrentBalance;
-};
+}
